288f85113c62f4f3a7b23069fb8fc5486462a483
Shunadesu <namp280918@gmail.com>
2025-02-19 12:17:30 +0700

    build: accessToken refreshToken logout

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,6 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 const {generateAccessToken, generateRefreshToken} = require('../middlewares/jwt')
-
 const asyncHandler = require('express-async-handler')
 
 const register = asyncHandler(async(req, res) => {
@@ -64,10 +64,43 @@ const getCurrent = asyncHandler(async(req, res) => {
 
 })
 
+const refreshAccessToken = asyncHandler(async(req, res) => {
+    // Lay token tu cookies
+    const cookie = req.cookies
+
+    //check xem co token hay khong
+    if(!cookie && !cookie.refreshToken) throw Error('No refresh token in cookies')
+    //check token con han hay khong
+    const rs = await jwt.verify(cookie.refreshToken, process.env.JWT_SECRET)
+    // Check xem token co khop voi token da luu trong db hay khong
+    const response = await User.findOne({_id: rs._id, refreshToken: cookie.refreshToken})
+    return res.status(200).json({
+        success: response ? true : false,
+        newAccessToken: response ? generateAccessToken(response._id, response.role) : 'RefreshToken not matched'
+    })
+})
+
+const logout = asyncHandler(async(req, res) => {
+    const cookie = req.cookies
+    if(!cookie || !cookie.refreshToken) throw new Error('No refreshToken in Cookie')
+    // Xoa cai refreshToken o db
+    await User.findOneAndUpdate({refreshToken: cookie.refreshToken}, {refreshToken: ''} , {new: true})
+    // Xoa refresh token o cookie trinh duyet
+    res.clearCookie('refreshToken',{
+        httpOnly: true,
+        secure: true
+    })
+    return res.status(200).json({
+        success: true,
+        mes: 'Logout successfully'
+    })
+})
 
 
 module.exports = {
     register, 
     login,
     getCurrent,
+    refreshAccessToken,
+    logout,
 }
--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -2,31 +2,36 @@ const jwt = require('jsonwebtoken')
 
 const asyncHandler = require ('express-async-handler')
 
-const verifyAccessToken = asyncHandler(async(req, res, next) => {
-    // Token thuong bat dau = chu bearer (dung de phan quyen)
+const verifyAccessToken = asyncHandler(async (req, res, next) => {
     // Bearer token
-
-    //Headers: {authorization: 'Bearer token}
-    if(req?.headers?.authorization?.startsWith('Bearer')){
-        const token = req.headers.authorization.split(' ')[1] // tao mang lay phan tu thu 2
+    // headers: { authorization: Bearer token}
+    if (req?.headers?.authorization?.startsWith('Bearer')) {
+        const token = req.headers.authorization.split(' ')[1]
         jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
-            if(err) return res.status(401).json({
-                succes: false,
-                mes: 'Invalid Web Token'
+            if (err) return res.status(401).json({
+                success: false,
+                mes: 'Invalid access token',
             })
-
-            console.log(decode)
             req.user = decode
             next()
         })
-    }else{
+    } else {
         return res.status(401).json({
-            succes: false,
-            mes: 'Require Authentication!!'
+            success: false,
+            mes: 'Require authentication!!!'
         })
     }
 })
-
+const isAdmin = asyncHandler((req, res, next) => {
+    const { role } = req.user
+    if (role !== 'admin')
+        return res.status(401).json({
+            success: false,
+            mes: ' REQUIRE ADMIN ROLE'
+        })
+    next()
+})
 module.exports = {
-    verifyAccessToken
+    verifyAccessToken,
+    isAdmin
 }
